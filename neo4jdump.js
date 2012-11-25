a50867cfd9d7727bd81ff96da236f6aac5a079a0
Jaakko-Heikki Heusala <jheusala@iki.fi>
2012-11-25 03:20:55 +0200

    Updated

--- neo4jdump.js
+++ neo4jdump.js
@@ -52,7 +52,7 @@ function fetch_nodes(list) {
 		var list = results.map(function (result) {
 			return {'id':result.nodes.id, 'data':result.nodes.data};
 		});
-		console.error('DEBUG: Got ' + list.length + ' items.');
+		//console.error('DEBUG: Got ' + list.length + ' items.');
 		defer.resolve(list);
 	}));
 	return defer.promise;
@@ -105,106 +105,133 @@ function fetch_rels(list) {
 			//console.error('DEBUG: result = ' + JSON.stringify(result));
 			return {'id':result.id, 'type':result.type, 'start':result.from_id, 'end':result.to_id, 'data':result.rel.data};
 		});
-		console.error('DEBUG: Got ' + list.length + ' items.');
+		//console.error('DEBUG: Got ' + list.length + ' items.');
 		defer.resolve(list);
 	}));
 	return defer.promise;
 }
 
-/* The Code */
-
-fetch_rel_id_list().then(errors.catchfail(function(id_list) {
-
-	/** Make sure JSON string has no new lines */
-	function format(str) {
-		if(str.indexOf("\n") === -1) return str;
-		console.error("DEBUG: Fixed some newlines in JSON!");
-		return str.replace(/\n/gm, "\\n");
-	}
+/** Make sure JSON string has no new lines */
+function format(str) {
+	if(str.indexOf("\n") === -1) return str;
+	console.error("DEBUG: Fixed some newlines in JSON!");
+	return str.replace(/\n/gm, "\\n");
+}
 	
+/* Handles next X nodes from the database */
+function dump_nodes(id_list, patch_amount, tell_progress) {
+	var defer = q.defer();
 	var total_items = id_list.length;
-	
-	/* Handles next X nodes from the database */
-	function dump_nodes(patch_amount) {
-		var defer = q.defer();
-		if(patch_amount < 1) {
-			defer.reject(new TypeError("patch_amount illegal"));
-			return defer.promise;
+	if(patch_amount < 1) {
+		defer.reject(new TypeError("patch_amount illegal"));
+		return defer.promise;
+	}
+	function do_nodes(){
+
+		if(id_list.length === 0) {
+			defer.resolve();
+			return;
 		}
-		function do_nodes(){
 	
-			if(id_list.length === 0) {
-				defer.resolve();
-				return;
-			}
+		var i = 0,
+	    max_len = id_list.length,
+	    next_len = max_len < patch_amount ? max_len : patch_amount,
+	    tmp = [];
+		for(; i<next_len; ++i) {
+			tmp.push(id_list.shift());
+		}
+		fetch_nodes(tmp).then(errors.catchfail(function(list) {
+			//console.error('DEBUG: Got ' + list.length + ' items - ' + id_list.length + ' items left of ' + total_items + ' (' + (100-Math.round((id_list.length/total_items)*100)) +'% complete).');
+			tell_progress((total_items-id_list.length)/total_items);
+			list.map(function(item){
+				console.log("N:" + item.id + ":" + format(JSON.stringify(item.data)) + ";");
+			});
+			
+			do_nodes();
+		}), function(err) {
+			defer.reject(err);
+		});
+		
+	} // do_next_nodes
+		
+	do_nodes();
+	return defer.promise;
+}
 	
-			var i = 0,
+/* Handles next X relations from the database */
+function dump_relations(id_list, patch_amount, tell_progress) {
+	var defer = q.defer();
+	var total_items = id_list.length;
+	if(patch_amount < 1) {
+		defer.reject(new TypeError("patch_amount illegal"));
+		return defer.promise;
+	}
+	function do_rels(){
+
+		if(id_list.length === 0) {
+			defer.resolve();
+			return;
+		}
+
+		var i = 0,
 		    max_len = id_list.length,
 		    next_len = max_len < patch_amount ? max_len : patch_amount,
 		    tmp = [];
-			for(; i<next_len; ++i) {
-				tmp.push(id_list.shift());
-			}
-			fetch_nodes(tmp).then(errors.catchfail(function(list) {
-				console.error('DEBUG: Got ' + list.length + ' items - ' + id_list.length + ' items left of ' + total_items + ' (' + Math.round((id_list.length/total_items)*100) +'%).');
-				list.map(function(item){
-					console.log("N:" + item.id + ":" + format(JSON.stringify(item.data)) + ";");
-				});
-				
-				do_nodes();
-			}), function(err) {
-				defer.reject(err);
-			});
-			
-		} // do_next_nodes
-		
-		do_nodes();
-		return defer.promise;
-	}
-	
-	/* Handles next X relations from the database */
-	function dump_relations(patch_amount) {
-		var defer = q.defer();
-		if(patch_amount < 1) {
-			defer.reject(new TypeError("patch_amount illegal"));
-			return defer.promise;
+		for(; i<next_len; ++i) {
+			tmp.push(id_list.shift());
 		}
-		function do_rels(){
-	
-			if(id_list.length === 0) {
-				defer.resolve();
-				return;
-			}
-	
-			var i = 0,
-			    max_len = id_list.length,
-			    next_len = max_len < patch_amount ? max_len : patch_amount,
-			    tmp = [];
-			for(; i<next_len; ++i) {
-				tmp.push(id_list.shift());
-			}
-			fetch_rels(tmp).then(errors.catchfail(function(list) {
-				console.error('DEBUG: Got ' + list.length + ' items - ' + id_list.length + ' items left of ' + total_items + ' (' + Math.round((id_list.length/total_items)*100) +'%).');
-				list.map(function(item){
-					console.log("R:" + item.id + ":" + format(item.type) + ":" + item.start + ":" + item.end + ":" + format(JSON.stringify(item.data)) + ";");
-				});
-				
-				do_rels();
-			}), function(err) {
-				defer.reject(err);
+		fetch_rels(tmp).then(errors.catchfail(function(list) {
+			//console.error('DEBUG: Got ' + list.length + ' items - ' + id_list.length + ' items left of ' + total_items + ' (' + (100-Math.round((id_list.length/total_items)*100)) +'% complete).');
+			tell_progress((total_items-id_list.length)/total_items);
+			list.map(function(item){
+				console.log("R:" + item.id + ":" + format(item.type) + ":" + item.start + ":" + item.end + ":" + format(JSON.stringify(item.data)) + ";");
 			});
 			
-		} // do_next_rels
+			do_rels();
+		}), function(err) {
+			defer.reject(err);
+		});
 		
-		do_rels();
-		return defer.promise;
-	}
+	} // do_next_rels
+		
+	do_rels();
+	return defer.promise;
+}
 	
-	dump_relations(500).then(function() {
-		console.error('ALL DONE.');
+/* The Code */
+
+var patch_size = parseInt(argv.size || 100, 10);
+
+fetch_node_id_list().then(errors.catchfail(function(node_id_list) {
+
+	var wheel = ['\\', '|', '/', '-'],
+	    i = 0;
+	process.stderr.write( 'Nodes: 0% (-)   ' );
+	dump_nodes(node_id_list, patch_size, function(progress) {
+		process.stderr.write( '\rNodes: ' + Math.round(progress*100) + '% (' + wheel[(i++)%4] + ')     ' );
+	}).then(function() {
+		process.stderr.write('\rNodes: 100%. ALL DONE.             \n');
+
+		fetch_rel_id_list().then(errors.catchfail(function(rel_id_list) {
+		
+			i = 0;
+			process.stderr.write( 'Relations: 0% (-)   ' );
+			dump_relations(rel_id_list, patch_size, function(progress) {
+				process.stderr.write( '\rRelations: ' + Math.round(progress*100) + '% (' + wheel[(i++)%4] + ')     ' );
+			}).then(function() {
+				process.stderr.write('\rRelations: 100%. ALL DONE.             \n');
+			}, function(err) {
+				errors.print(err);
+			});
+		
+		}), function(err) {
+			errors.print(err);
+		});
+
 	}, function(err) {
 		errors.print(err);
 	});
+	
 
 }), function(err) {
 	errors.print(err);
